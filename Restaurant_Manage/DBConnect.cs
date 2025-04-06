333119db92570e78d879e266a576fac64e4e65ab
MurielJesalFernandes <murieljesalfernandes@gmail.com>
2025-04-06 22:16:58 +0530

    Console.WriteLine modified

---
+++ Restaurant_Manage/DBConnect.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Restaurant_Manage
+{
+    class DBConnect
+    {
+        private SqlConnection con = new SqlConnection(@"Data Source=MURIEL\SQLEXPRESS;Initial Catalog=RestMangDB;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+        public SqlConnection GetCon()
+        { return con; }
+        public void OpenCon()
+        {
+            if(con.State==ConnectionState.Closed)
+            {  con.Open(); }
+        }
+        public void CloseCon()
+        {
+            if (con.State == ConnectionState.Open)
+            { con.Close(); }
+        }
+    }
+}
--- Restaurant_Manage/Form1.cs
+++ Restaurant_Manage/Form1.cs
@@ -7,14 +7,71 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Restaurant_Manage
 {
     public partial class Form1: Form
     {
+        DBConnect dbCon = new DBConnect();
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text == String.Empty)
+                {
+                    MessageBox.Show("Please enter valid Username", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textBox1.Focus();
+                    return;
+                }
+                if (textBox2.Text==String.Empty)
+                {
+                    MessageBox.Show("Please enter valid Password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textBox2.Focus();
+                    return;
+                }
+                if (textBox1.Text != String.Empty && textBox2.Text != String.Empty)
+                {
+                    //login code
+
+                }
+                else
+                {
+                    MessageBox.Show("Please enter Username or Password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textBox1.Clear();
+                    textBox2.Clear();
+                }
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        
     }
 }
